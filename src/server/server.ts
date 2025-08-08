aba7f7541925560e821acd9daf3878fe0363e736
GabrielSoman <68242795+GabrielSoman@users.noreply.github.com>
2025-08-08 15:34:36 -0300

    Corrigir erro import.meta no servidor

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { createRequire } from 'module';
 import apiRoutes from './routes/api';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+const __dirname = path.dirname(require.resolve('./server.js'));
 
 const app = express();
 const PORT = process.env.PORT || 80;
