1f0c782dead9b86a4ae5b56d035fe62a6af8f466
GabrielSoman <68242795+GabrielSoman@users.noreply.github.com>
2025-08-08 15:00:03 -0300

    Configurar aplicação para porta 80

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 80,
+    host: true
+  },
+  preview: {
+    port: 80,
+    host: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
